67f4f692ae2dae5158cfc40fbc8fb4f28bd5e678
SAtacker <shreyasatre16@gmail.com>
2021-07-24 15:42:58 +0530

    Partitions and their stats [![asciicast](https://asciinema.org/a/426969.svg)](https://asciinema.org/a/426969)

--- src/ui/panel/emmc/emmc_impl.cpp
+++ src/ui/panel/emmc/emmc_impl.cpp
@@ -1,3 +1,7 @@
+#include <pwd.h>
+#include <unistd.h>
+#include <filesystem>
+#include <fstream>
 #include "ftxui/dom/elements.hpp"
 #include "ui/panel/panel.hpp"
 
@@ -8,16 +12,56 @@ namespace ui {
 class EMMCImpl : public PanelBase {
  public:
   EMMCImpl() {
-    Add(Renderer([] {
-      return vbox({
-          hbox(text(L"  SD card:["), gauge(0.5f), text(L"] (50%)")),
-          hbox(text(L"EMMC card:["), gauge(0.75f), text(L"] (75%)")),
-      });
-      return gauge(0.75);
+    Add(Renderer([&] {
+      Elements sizeList;
+      updateBlocks();
+
+      for (size_t i = 0; i < blocks.size(); i++) {
+        auto free_space = std::to_wstring(
+            std::filesystem::space("/dev/" + to_string(blocks[i])).free);
+        auto cap = std::to_wstring(
+            std::filesystem::space("/dev/" + to_string(blocks[i])).capacity);
+        auto avail = std::to_wstring(
+            std::filesystem::space("/dev/" + to_string(blocks[i])).available);
+        sizeList.push_back(hbox({
+                               text(L" Free: " + free_space) | flex,
+                               text(L" Cap: " + cap) | flex,
+                               text(L" Avail:" + avail) | flex,
+                           }) |
+                           border);
+      }
+      auto currentUserPath = std::string(getpwuid(geteuid())->pw_dir);
+      auto free_space =
+          std::to_wstring(std::filesystem::space(currentUserPath).free);
+      auto cap =
+          std::to_wstring(std::filesystem::space(currentUserPath).capacity);
+      auto avail =
+          std::to_wstring(std::filesystem::space(currentUserPath).available);
+      sizeList.push_back(hbox({
+                             text(to_wstring(currentUserPath)) | flex,
+                             text(L" Free: " + free_space) | flex,
+                             text(L" Cap: " + cap) | flex,
+                             text(L" Avail:" + avail) | flex,
+                         }) |
+                         border);
+      return vbox(sizeList);
     }));
   }
   ~EMMCImpl() = default;
   std::wstring Title() override { return L"EMMC and MicroSD stats"; }
+
+ private:
+  void updateBlocks() {
+    std::string path = "/sys/block/";
+    blocks.clear();
+    for (const auto& entry : std::filesystem::directory_iterator(path)) {
+      auto p = std::string(entry.path());
+      auto block = p.substr(p.find_last_of("/") + 1);
+      blocks.push_back(to_wstring(block));
+    }
+  }
+
+  std::vector<std::wstring> blocks;
 };
 
 namespace panel {
