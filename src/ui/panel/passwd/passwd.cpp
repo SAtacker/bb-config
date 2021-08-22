0003a3f3acd66542425060b2a7797c37133ccb0d
ArthurSonzogni <sonzogniarthur@gmail.com>
2021-08-22 13:58:51 +0200

    Add "Hide password" button.
    + Add an "Hide password" button.
    + Upgrade FTXUI version to include:
      https://github.com/ArthurSonzogni/FTXUI/commit/b95a7a4c6b8e89f5288a96d8b2c0461ad4a69f77
    + And various minor improvements.

--- src/ui/panel/passwd/passwd.cpp
+++ src/ui/panel/passwd/passwd.cpp
@@ -12,58 +12,55 @@ namespace {
 
 class passwdImpl : public PanelBase {
  public:
-  passwdImpl() : user(getlogin()), view(false) {
-    InputOption inp_opt;
-    inp_opt.password = true;
-    inp_opt.cursor_position = 0;
-    inp_opt.on_enter = [&] { view = !view; };
-    input_p = Input(&pass, "Current Password", inp_opt);
-    input_np = Input(&new_pass, "New Password", inp_opt);
-    change_ = Button("Change", [&] {
-      auto cmd = "passwd " + user;
+  passwdImpl() : user_(getlogin()) {
+    input_options_.password = &hide_password_;
+    input_password_old_ =
+        Input(&password_old_, "Current Password", &input_options_);
+    input_password_new_ =
+        Input(&password_new_, "New Password", &input_options_);
+    hide_password_checkbox_ = Checkbox("Hide password", &hide_password_);
+    apply_button_ = Button("Apply", [&] {
+      auto cmd = "passwd " + user_;
       FILE* fp = popen(cmd.c_str(), "w");
-      fprintf(fp, "%s\n", pass.data());
-      fprintf(fp, "%s\n", new_pass.data());
-      fprintf(fp, "%s\n", new_pass.data());
+      fprintf(fp, "%s\n", password_old_.data());
+      fprintf(fp, "%s\n", password_new_.data());
+      fprintf(fp, "%s\n", password_new_.data());
       pclose(fp);
     });
-    page = Renderer(Container::Vertical({
-                        input_p,
-                        input_np,
-                        change_,
-                    }),
-                    [&] {
-                      if (view) {
-                        return vbox({
-                            text("User: " + user),
-                            text("Pass: " + pass),
-                            text("New : " + new_pass),
-                            input_p->Render(),
-                            input_np->Render(),
-                            change_->Render(),
-                        });
-                      }
-                      return vbox({
-                          text("User: " + user),
-                          input_p->Render(),
-                          input_np->Render(),
-                          change_->Render(),
-                      });
-                    });
-    Add(page);
-  };
+
+    auto layout = Container::Vertical({
+        input_password_old_,
+        input_password_new_,
+        hide_password_checkbox_,
+        apply_button_,
+    });
+
+    layout = Renderer(layout, [&] {
+      return vbox({
+          hbox(text("User:"), text(user_)),
+          hbox(text("Old password:"), input_password_old_->Render()),
+          hbox(text("New password:"), input_password_new_->Render()),
+          hide_password_checkbox_->Render(),
+          apply_button_->Render(),
+      });
+    });
+
+    Add(layout);
+  }
   ~passwdImpl() = default;
   std::string Title() override { return "Password"; }
 
  private:
-  Component input_p;
-  Component input_np;
-  Component change_;
-  Component page;
-  std::string pass;
-  std::string new_pass;
-  std::string user;
-  bool view;
+  std::string password_old_;
+  std::string password_new_;
+  std::string user_;
+
+  Component input_password_old_;
+  Component input_password_new_;
+  Component hide_password_checkbox_;
+  Component apply_button_;
+  InputOption input_options_;
+  bool hide_password_ = false;
 };
 
 }  // namespace
