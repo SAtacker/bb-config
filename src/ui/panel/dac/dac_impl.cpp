6bb0d342a73edf3a4b35bcd4a1a65a7acd915702
jiande <jiande2020@protonmail.com>
2022-07-15 12:17:52 +0800

    FindPWMs

--- src/ui/panel/dac/dac_impl.cpp
+++ src/ui/panel/dac/dac_impl.cpp
@@ -6,19 +6,37 @@
 #include "ftxui/dom/elements.hpp"
 #include "ui/panel/panel.hpp"
 
-const std::string PWM_FILE_PATH = "/sys/class/pwm";
+const std::string PWM_FILE_PATH = "/sys/class/pwm/";
 
 using namespace ftxui;
+
+std::vector<std::string> FindPWMs() {
+  std::vector<std::string> names;
+
+  procxx::process ls{"ls"};
+  ls.add_argument("-C1");
+  ls.add_argument("PWM_FILE_PATH");
+  ls.add_argument("| grep pwm-");
+  ls.exec();
+
+  std::string name;
+  while (std::getline(ls.output(), name))
+    names.push_back(name);
+  return names;
+}
 
 namespace ui {
 class DACImpl : public PanelBase {
     public:
         DACImpl() {
-            for (const auto& it : std::filesystem::directory_iterator(PWM_FILE_PATH)) {
-                std::string pwm_path = it.path();
-                if (pwm_path.find("pwm-0"))
-                    v_DAC_pin_.push_back(pwm_path);
+            for (auto name : FindPWMs()) {
+                pwm_path.push_back(name);
             }
+            // for (const auto& it : std::filesystem::directory_iterator(PWM_FILE_PATH)) {
+            //     std::string pwm_path = it.path();
+            //     if (pwm_path.find("pwm-0"))
+            //         v_DAC_pin_.push_back(pwm_path);
+            // }
             
             Component page = Renderer(Container::Vertical({
                                   Container::Horizontal({
