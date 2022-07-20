5def53f3fc606007e83b1ee5397fc9c999d88107
jiande <jiande2020@protonmail.com>
2022-07-20 15:22:45 +0800

    update adc

--- src/ui/panel/adc/adc_impl.cpp
+++ src/ui/panel/adc/adc_impl.cpp
@@ -1,67 +1,125 @@
 #include <fstream>
 #include <sstream>
+#include <regex>
+#include <cmath>
+#include <functional>  
+#include <memory>  
+#include <chrono> 
+#include <cstdlib>
+#include <vector>
+#include <filesystem>
+
 #include "ftxui/component/component.hpp"
 #include "ftxui/dom/elements.hpp"
 #include "process.hpp"
 #include "ui/panel/panel.hpp"
 
-#define ADC_PATH "/sys/bus/iio/devices/iio\\:device0/"
-
 using namespace ftxui;
+using namespace std::chrono_literals;
 
 namespace ui {
 
-std::vector<std::string> FindADCs() {
+const std::string Analog_Path = "/sys/bus/iio/devices/iio:device0";
+
+std::vector<std::string> FindAnalogs() {
   std::vector<std::string> names;
 
-  procxx::process ls{"ls"};
-  ls.add_argument("-C1");
-  ls.add_argument("/sys/bus/iio/devices/iio:device0/");
-  ls.exec();
+  for (const auto& it : std::filesystem::directory_iterator(Analog_Path)) {
+    std::stringstream ss(it.path());
+    std::string name;
 
-  std::string name;
-  while (std::getline(ls.output(), name))
-    names.push_back(name);
+    while(ss.good()) {
+      getline(ss, name, '/');
+    }
+
+    if ( std::regex_match(name, std::regex("(in_voltage)([0-9]+)(_raw)")) )
+      names.push_back(name);
+  }
+
   return names;
 }
+
+class Graph {
+  public:
+    std::vector<int> operator()(int width, int height) const {
+      std::vector<int> output(width);
+      for (int i = 0; i < width; ++i) {
+        if ( i >= (int)data_vect.size() )
+          break;
+
+        float v = 0;
+        v *= height;
+        v += data_vect[i];   
+
+        output[i] = static_cast<int>(v);
+      }
+      return output;
+    }
+    int shift = 0;
+
+    void update() {
+      int value;
+      std::ifstream(Analog_Path + name_) >> value;
+      data_vect.erase(data_vect.begin());
+      data_vect.push_back(value);
+    }
+
+    void set_name(std::string input) {
+      name_ = input;
+    }
+
+  private:
+    std::vector<int> data_vect = std::vector<int>(200, 10);
+    std::string name_;
+};
 
 class adcImpl : public PanelBase {
   public:
     adcImpl() {
-      for (auto name : FindADCs()) {
-        pin_.push_back(name);
+      for (auto name : FindAnalogs()) {
+        analog_pin_.push_back(name);
       }
-      Add(Container::Vertical({
-        analog_radiobox_,
-        sample_frequency_,
-        button_generate_,
+
+      Add( Container::Vertical( {
+        radio_,
       }));
     }
-    ~adcImpl() = default;
+
+    ~adcImpl() override = default;
 
   private:
-    Element Render() override {
-      return vbox({
-                text("Select an Analogue Pin:"),
-                hbox(text(" "), analog_radiobox_->Render()),
-                separator(),
-                hbox(text(" "), sample_frequency_->Render()),
-                separator(),
-                button_generate_->Render(),
-              })|
-              vscroll_indicator | frame;
-    }
-
     std::string Title() override { return "ADC"; }
+    int selected = 0;
+    std::vector<std::string> analog_pin_;  
+    Graph my_graph;
+    Component button_ = Button("Button", [this] {} );
+    Component radio_ = Radiobox(&analog_pin_, &selected); 
 
-    int selected_analog_pin = 0;
-    int selected_frequency = 0;
-    std::vector<std::string> pin_;
-    std::vector<std::string> fre_ {"1kHz", "2kHz"};
+    Element Render() override {
+      my_graph.shift++;
+      my_graph.update();
+      const auto sleep_time = 0.03s;
+      std::this_thread::sleep_for(sleep_time);
 
-    Component analog_radiobox_ = Radiobox(&pin_, &selected_analog_pin);
-    Component sample_frequency_ = Dropdown(&fre_, &selected_frequency);
-    Component button_generate_ = Button("Generate", [this] {});
+      auto graph_page = vbox({
+          text("Analog [Mhz]") | hcenter,
+          hbox({
+              vbox({
+                  text("2400 "),
+                  filler(),
+                  text("1200 "),
+                  filler(),
+                  text("0 "),
+              }),
+              graph(std::ref(my_graph)) | flex,
+          }) | flex,
+      });
+
+      return vbox({
+        radio_->Render(),
+        graph_page,
+      }) | vscroll_indicator | frame;
+    }
 };
 
 namespace panel {
